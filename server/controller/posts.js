bdd91f69fa617ccc6c117f5e691afc1ce1f5561f
boxuan <boxuan@ualberta.ca>
2021-05-18 15:15:58 -0600

    finish getposts router controller

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,17 @@
-export const getPosts = (req, res) => {
-    res.send('THIS IS WORK');
+import PostMessage from '../models/postMessage.js';
+
+
+
+export const getPosts = async (req, res) => {
+    
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).jason(postMessages);
+    }catch (error) {
+        res.stauts(404).json({message: error.message});
+    }
+}
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 }
--- server/route/posts.js
+++ server/route/posts.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {getPosts} from '../controller/posts.js';
+import {getPosts, createPost} from '../controller/posts.js';
 const router = express.Router();
 
 router.get('/' , getPosts);
+router.post('/', createPost);
 
 export default router;
